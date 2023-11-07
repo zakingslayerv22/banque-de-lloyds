767e03beb6d2d822a0d05735c54a257b1cd2e87b
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-11-08 00:54:43 +0100

    Fix the basic features
    If it breaks at any point from here,
    start by looking for this file
    
    clientlibs-js.min.37932920ddffe2fca192351b122d2d1d
    
    and replace it with the original. I put it in the
    /assets directory for easy access.
    
    You will still see it if you search for it globally
    in the lloyds directory,

---
+++ www_lloydsbank_com.js
@@ -0,0 +1,1 @@
+utag.globals.dle.enrichments['www_lloydsbank_com'] = {"action":"container","actions":[{"action":"lazy-container","actions":[{"action":"more-santa","key":"public_loans_calc","criteria":["pathname equals /loans/loan-calculator.html"]},{"action":"more-santa","key":"public_carfinance_calc","criteria":["pathname equals /car-finance/calculator.html"]}]}]}
